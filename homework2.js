c8b9a33df6a3e7d9dc2a9351db2cbb7f463dcacc
Jan Dennison <janice.dennison@inventivhealth.com>
2016-05-14 17:30:27 -0400

    split classes into files

--- homework2.js
+++
@@ -1,7 +0,0 @@
-// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
-
-//  Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-
-// Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
-
-Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
